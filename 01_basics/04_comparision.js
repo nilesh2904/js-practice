ce0d9c404d410f492c762815c56a91f39c4526c7
NILESH HUMBE <nileshhumbe04@gmail.com>
2024-06-27 07:58:33 +0000

    compare

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -5,17 +5,19 @@
 // console.log(2 != 1);
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+console.log("2" > 1);
+console.log("02" > 1);
 
 console.log(null > 0);
 console.log(null == 0);
 console.log(null >= 0);
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
 
 // === 
 
 console.log("2" === 2);
+
+// in this it does not convert the datatype of the value, due to which it will return false has a answer.
