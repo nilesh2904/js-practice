243dea88e724701c0541c646a7c12caf3027ebb1
NILESH HUMBE <nileshhumbe04@gmail.com>
2024-06-28 18:00:05 +0000

    array 2

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,21 +1,21 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
-// marvel_heros.push(dc_heros)
+// marvel_heros.push(dc_heros)   // using push it insert mentioned array same as it is into the existing array.
 
 // console.log(marvel_heros);
 // console.log(marvel_heros[3][1]);
 
-// const allHeros = marvel_heros.concat(dc_heros)
+// const allHeros = marvel_heros.concat(dc_heros)  //using concat it merged all values into new array and not in existing array.
 // console.log(allHeros);
 
-const all_new_heros = [...marvel_heros, ...dc_heros]
+const all_new_heros = [...marvel_heros, ...dc_heros] // using this it spread the multiple arrays and give it in single array
 
 // console.log(all_new_heros);
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
-const real_another_array = another_array.flat(Infinity)
+const real_another_array = another_array.flat(Infinity)  // using flat, it returns a new array with all sub-array elements concatenated into it recursively up to the specified depth.
 console.log(real_another_array);
 
 
