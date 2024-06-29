e0386b27914ed453aae6e05027185265b4b98fc4
NILESH HUMBE <nileshhumbe04@gmail.com>
2024-06-29 06:48:12 +0000

    objects and JSON API

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -1,5 +1,5 @@
-// const tinderUser = new Object()
-const tinderUser = {}
+// const tinderUser = new Object()    //singleton object
+const tinderUser = {}                 // non singleton object
 
 tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
@@ -11,22 +11,23 @@ const regularUser = {
     email: "some@gmail.com",
     fullname: {
         userfullname: {
-            firstname: "hitesh",
-            lastname: "choudhary"
+            firstname: "Nilesh",
+            lastname: "Humbe"
         }
     }
 }
 
 // console.log(regularUser.fullname.userfullname.firstname);
+// console.log(regularUser.fullname.userfullname.firstname, regularUser.fullname.userfullname.lastname);
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
 // const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+// const obj3 = Object.assign({}, obj1, obj2, obj4)   // it is static method copies all enumerable (repetative/can be looped) own properties from one or more source objects to a target object. {} is target and other all are source
 
-const obj3 = {...obj1, ...obj2}
+const obj3 = {...obj1, ...obj2}  // spread method and it is mostly used
 // console.log(obj3);
 
 
@@ -50,7 +51,7 @@ users[1].email
 
 // console.log(Object.keys(tinderUser));
 // console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+// console.log(Object.entries(tinderUser));  // it returns array into array and give seperated key values pair in array
 
 // console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
@@ -60,17 +61,19 @@ const course = {
     price: "999",
     courseInstructor: "hitesh"
 }
+// console.log(course.courseInstructor);
+
 
 // course.courseInstructor
 
-const {courseInstructor: instructor} = course
+const {courseInstructor: instructor} = course           // deconstructor of object value
 
-// console.log(courseInstructor);
-console.log(instructor);
+// console.log(courseInstructor); 
+console.log(instructor);  
 
 // {
 //     "name": "hitesh",
-//     "coursename": "js in hindi",
+//     "coursename": "js in hindi",          // json format
 //     "price": "free"
 // }
 
