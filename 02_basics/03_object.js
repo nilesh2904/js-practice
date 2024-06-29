6983f8e394bf9abc2376f0aadfb482ee680224f2
NILESH HUMBE <nileshhumbe04@gmail.com>
2024-06-29 06:00:45 +0000

    objects part1

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -1,5 +1,7 @@
 // singleton
-// Object.create
+// Object.create   // constructor method
+
+
 
 // object literals
 
@@ -18,12 +20,12 @@ const JsUser = {
 }
 
 // console.log(JsUser.email)
-// console.log(JsUser["email"])
+// console.log(JsUser["email"])   // it excess it as string so need to mentioned in '' and it is a prefer method
 // console.log(JsUser["full name"])
 // console.log(JsUser[mySym])
 
 JsUser.email = "hitesh@chatgpt.com"
-// Object.freeze(JsUser)
+// Object.freeze(JsUser)  // using freeze, we can freeze the function, so that if another value pass after freeze it wont be changed.
 JsUser.email = "hitesh@microsoft.com"
 // console.log(JsUser);
 
@@ -31,7 +33,7 @@ JsUser.greeting = function(){
     console.log("Hello JS user");
 }
 JsUser.greetingTwo = function(){
-    console.log(`Hello JS user, ${this.name}`);
+    console.log(`Hello JS user, ${this.name}`); // this gives reference to the same object and give access to the properties it have. 
 }
 
 console.log(JsUser.greeting());
