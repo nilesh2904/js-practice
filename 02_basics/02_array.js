ab82c48730f34bcea5ed342bdef71e6651e598a0
NILESH HUMBE <nileshhumbe04@gmail.com>
2024-06-29 05:27:32 +0000

    arrays2

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -21,11 +21,11 @@ console.log(real_another_array);
 
 
 console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+console.log(Array.from("Hitesh"))  // it convert values into array
+console.log(Array.from({name: "hitesh"})) // interesting. it will give empty array. in this case we need to specify by using what array should be made like by key or its value.
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3));
+console.log(Array.of(score1, score2, score3));  // using of it converts values into array.
