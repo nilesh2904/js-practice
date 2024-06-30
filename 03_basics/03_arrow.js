49e380fea82dba9c6b2fd2d73eddb966c37bda5d
NILESH HUMBE <nileshhumbe04@gmail.com>
2024-06-30 07:48:08 +0000

    this and arrow function

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -3,7 +3,7 @@ const user = {
     price: 999,
 
     welcomeMessage: function() {
-        console.log(`${this.username} , welcome to website`);
+        console.log(`${this.username} , welcome to website`);               // here this is used as current context that holds the current value of {}
         console.log(this);
     }
 
@@ -15,19 +15,19 @@ const user = {
 
 // console.log(this);
 
-// function chai(){
+// function chai(){                                        // this is cannot used directly in the function. it has to be declare object values first.
 //     let username = "hitesh"
 //     console.log(this.username);
 // }
 
 // chai()
 
-// const chai = function () {
+// const chai = function () {                            // it will returned undefined, as the object value is not declare first.
 //     let username = "hitesh"
 //     console.log(this.username);
 // }
 
-const chai =  () => {
+const chai =  () => {                              // 
     let username = "hitesh"
     console.log(this);
 }
@@ -35,13 +35,13 @@ const chai =  () => {
 
 // chai()
 
-// const addTwo = (num1, num2) => {
+// const addTwo = (num1, num2) => {               // basic arrow function
 //     return num1 + num2
 // }
 
-// const addTwo = (num1, num2) =>  num1 + num2
+// const addTwo = (num1, num2) =>  num1 + num2          // implicity return
 
-// const addTwo = (num1, num2) => ( num1 + num2 )
+// const addTwo = (num1, num2) => ( num1 + num2 )       // this method is used mostly in react
 
 const addTwo = (num1, num2) => ({username: "hitesh"})
 
