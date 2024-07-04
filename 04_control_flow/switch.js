8afa7ec86ea5a0908e7d5c843400cf5c696a32da
NILESH HUMBE <nileshhumbe04@gmail.com>
2024-07-04 07:02:20 +0000

    controlflow_switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,31 @@
+// in this if we dont use break it will print all the value after it match the actual value, but it will not print the default value.
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
